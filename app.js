be327a3b53e5a200bab3802d8e5b17dfaf27bad1
pinetree <kirill.sosnin@gmail.com>
2018-11-08 15:34:08 +0300

    Added create contact and release contact methods

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const pathList = {
   ADS_LIST_URL: '/api/ads/',
   sellBtcCurrency: (currency, payment_method) => `/sell-bitcoins-online/${currency}/${payment_method}/.json`,
   // sellBtcCountry: (countrycode, country_name, payment_method) => `/sell-bitcoins-online/${countrycode}/${country_name}/${payment_method}/.json`
+  createContact: ad_id => `/api/contact_create/${ad_id}/`,
+  releaseContact: contact_id => `/api/contact_release/${contact_id}/`
 }
 
 const getCurrencies = async () => {
@@ -21,7 +23,6 @@ const getMyAds = async () => {
 const getAds = async () => {
   const response = await API.get( pathList.sellBtcCurrency('RUB', 'national-bank-transfer') )
   // const response = await API.get( pathList.sellBtcCountry('RU', 'Russian_Federation', 'national-bank-transfer') )
-  // console.log(response.data.ad_list)
 
   // todo: use lodash _get
   return response ? response.data.ad_list : null
@@ -42,12 +43,33 @@ const getOptimalAd = async conditions => {
   }
 }
 
+/**
+ *
+ * @param ad_id advertisement ID.
+ * @param amount Amount is a number in the advertisement's fiat currency.
+ * @param message Optional message to send to the advertiser.
+ * @param optional ONLINE_BUY advertisements may have additional fields you need to provide depending on the advertisement's payment method (online_provider).
+ * @returns {Promise<void>} Returns the API URL to the newly created contact at actions.contact_url. Whether the contact was able to be funded automatically is indicated at data.funded. Only non-floating LOCAL_SELL may return unfunded, all other trade types either fund or fail.
+ */
+const createContact = async (ad_id, amount, message, optional) => {
+  const response = await API.post(pathList.createContact(ad_id), { amount, message, ...optional  })
+
+  console.log(response)
+}
+
+/**
+ * Releases Bitcoin trades specified by ID {contact_id}. If the release was successful a message is returned on the data key.
+ * @param contact_id
+ * @returns {Promise<void>}
+ */
+const contactRelease = async contact_id => {
+  const response = await API.post(pathList.releaseContact(contact_id))
+
+  console.log(response)
+}
+
 // getCurrencies()
 // getAds()
-getOptimalAd({
-  amount: 2,
-  shouldEquals: {
-    trade_type: 'ONLINE_BUY',
-    city: ''
-  }
-}) // 2 BTC
+// getOptimalAd({ amount: 2,  shouldEquals: { trade_type: 'ONLINE_BUY', city: '' } }) // 2 BTC
+// createContact(873397, '45.0', 'Перевести на Qiwi', { 'details-phone_number': '+79092547959' })
+// contactRelease(31659423)
